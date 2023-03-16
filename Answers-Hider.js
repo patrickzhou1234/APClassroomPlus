6748977a4eec44e65ed635ed420b4cb9ad3da30e
Patrick Zhou <patrickchang44543@gmail.com>
2023-03-15 19:05:55 -0700

    Better when clicked.

--- Answers-Hider.js
+++ Answers-Hider.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://apclassroom.collegeboard.org/*
+// @match        https://apclassroom.collegeboard.org/*/assessments/results/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=collegeboard.org
 // @grant        none
 // ==/UserScript==
@@ -12,8 +12,16 @@
 window.onkeydown = function(event) {
     if (event.keyCode=="192") {
         document.querySelectorAll("div.LearnosityDistractor.--valid").forEach(el => el.remove());
-        document.querySelectorAll("div.icon.--correct").forEach(el => el.remove());
-        document.querySelectorAll("div.AccessibilityWrapper.mcq-option.--correct").forEach(el => el.classList.remove("--correct"));
+        document.querySelectorAll("div.icon.--correct").forEach(el => {
+            el.style.display = "none";
+            el.parentElement.parentElement.onclick = () => {
+                el.style.display = "block";
+                el.parentElement.parentElement.classList.add("--correct");
+            }
+        });
+        document.querySelectorAll("div.AccessibilityWrapper.mcq-option.--correct").forEach(el => {
+            el.classList.remove("--correct");
+        });
         document.querySelectorAll("div.letter.--chosen").forEach(el => el.classList.remove("--chosen"));
     }
 }
