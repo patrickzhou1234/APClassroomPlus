aa3a26e0b288628b9871f20e053e38092a12abbe
Patrick Zhou <patrickchang44543@gmail.com>
2024-09-25 13:14:14 -0700

    Update CopyButton.js

--- CopyButton.js
+++ CopyButton.js
@@ -15,6 +15,19 @@ setInterval(() => {
         if (!el.parentElement.textContent.includes("Copy")) {
             btn = document.createElement("button");
             btn.textContent = "Copy";
+            btn.setAttribute("class","font-normal font-content cursor-pointer rounded-20 px-4.2 py-2.8 h-12 whitespace-nowrap bg-bluebook-blue focus:outline-none text-white border-[1px] border-bluebook-blue transition-shadow duration-[250ms] ease-in-out hover:border-bluebook-dark-blue focus:shadow-[0_0_0_2px] focus:shadow-bluebook-blue focus:underline focus:border-white h-[40px] w-[78px] !font-bold text-[14px] ml-[12px] inline-flex items-center justify-center");
+            el.parentElement.appendChild(btn);
+            btn.onclick = () => {
+                navigator.clipboard.writeText(el.textContent);
+                Swal.fire("Copied!", "", "success");
+            };
+        }
+    });
+    document.querySelectorAll(".passage").forEach((el) => {
+        if (!el.parentElement.textContent.includes("Copy")) {
+            btn = document.createElement("button");
+            btn.textContent = "Copy";
+            btn.setAttribute("class","font-normal font-content cursor-pointer rounded-20 px-4.2 py-2.8 h-12 whitespace-nowrap bg-bluebook-blue focus:outline-none text-white border-[1px] border-bluebook-blue transition-shadow duration-[250ms] ease-in-out hover:border-bluebook-dark-blue focus:shadow-[0_0_0_2px] focus:shadow-bluebook-blue focus:underline focus:border-white h-[40px] w-[78px] !font-bold text-[14px] ml-[12px] inline-flex items-center justify-center");
             el.parentElement.appendChild(btn);
             btn.onclick = () => {
                 navigator.clipboard.writeText(el.textContent);
