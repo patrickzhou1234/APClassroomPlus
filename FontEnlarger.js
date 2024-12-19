51ac2980ba423fd80239063605a6894d05c7701d
Patrick Zhou <patrickchang44543@gmail.com>
2024-12-18 18:57:47 -0800

    Create FontEnlarger.js

---
+++ FontEnlarger.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         AP Classroom Assignment Text Enlarger
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://apstudents.collegeboard.org/*
+// @match        https://apclassroom.collegeboard.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=collegeboard.org
+// @grant        none
+// ==/UserScript==
+
+setInterval(() => {
+    document.querySelectorAll(".stimulus_reference").forEach((el)=>{
+        el.style.fontSize="2vmin";
+    });
+    document.querySelectorAll(".stem_paragraph").forEach((el)=>{
+        el.style.fontSize="2vmin";
+    });
+    document.querySelectorAll(".lrn-possible-answer").forEach((el)=>{
+        el.style.fontSize="2vmin";
+    });
+    document.querySelectorAll(".choice_paragraph").forEach((el)=>{
+        el.style.fontSize="2vmin";
+    });
+}, 500);
